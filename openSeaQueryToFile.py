8c5d4c434b79f804f5a0de6603fa3b64af50b21c
Michael Gallegos <michael@mgallegos.dev>
2023-02-25 22:17:11 -0700

    fix: update apiKey in openSea file
    lol

--- openSeaQueryToFile.py
+++ openSeaQueryToFile.py
@@ -26,7 +26,7 @@ for i in range(16708664, 16658664, -300):
     # print("query: " + salesPerThousandBlocksQuery)
 
     urlGateway = 'https://gateway.thegraph.com/api/'
-    apiKey = '51d7a63c993112346960cd6c837faa11' # REPLACE THIS WITH YOUR OWN API KEY
+    apiKey = 'MYKEY' # REPLACE THIS WITH YOUR OWN API KEY
     urlRoute = '/subgraphs/id/G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c'
     url = urlGateway + apiKey + urlRoute
     r = requests.post(url, json={'query': salesPerThousandBlocksQuery})
